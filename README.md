1010d3641bbc545cfc37d7ef18cadda81b312b01
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2019-09-13 17:19:48 +0530

    fix: compress images, svg and markup cleanup

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ### A product landing page.
-A minimalistic product landing page. 
 
-Clean up To Do's:
-- [ ] Refine CSS and HTML
-- [ ] Add SEO and social tags
+A minimalistic product landing page for cs50 pset and freeCodeCamp project.
+
+To Do's:
+
+- [ ] Make it responsive
+- [ ] Add SEO and social meta tags
 - [ ] Add favicon
+- [ ] Validate HTML and CSS using https://validator.w3.org/
--- js/specs.js
+++ js/specs.js
@@ -2,7 +2,6 @@
 document.querySelector(".base-btn").addEventListener("click", function() {
   document.querySelector(".pro-specs-head").style.display = "none";
   document.querySelector(".pro-specs").style.display = "none";
-
   document.querySelector(".base-specs-head").style.display = "block";
   document.querySelector(".base-specs").style.display = "table";
 });
@@ -10,7 +9,6 @@ document.querySelector(".base-btn").addEventListener("click", function() {
 document.querySelector(".pro-btn").addEventListener("click", function() {
   document.querySelector(".base-specs-head").style.display = "none";
   document.querySelector(".base-specs").style.display = "none";
-
   document.querySelector(".pro-specs-head").style.display = "block";
   document.querySelector(".pro-specs").style.display = "table";
 });
